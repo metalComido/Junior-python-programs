d184e13471ea72ba94dac5475ca0497e104378f4
Leobardo Ernesto Sanchez Cruz <Leobardo.Sanchez@uabc.edu.mx>
2021-02-25 14:59:29 -0800

    11 python junior can do version 2

--- Biography info/Biography_info.py
+++ Biography info/Biography_info.py
@@ -1,4 +1,11 @@
-Name = str(input("Whats is you name"))
+Name = str(input("Whats is you name: "))
 print("Add you birthday")
-day , month , year = int(input("day:")) , int(print("month:")),int(print("year:"))
+day , month , year = int(input("day:")) , str(input("month:")), int(input("year:"))
+print("Add your adress")
+adress,city = str(input("Adress: ")),str(input("city: "))
+goal= str(input("add yout personal goal:"))
 
+print("- Name: {}".format(Name))
+print("- Date of birth: {} {},{}".format(month, day, year))
+print("- Address: {}, {}".format(adress,city))
+print("- Personal goals: {}".format(goal))
