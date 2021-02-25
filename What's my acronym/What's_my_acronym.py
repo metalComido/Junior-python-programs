a9b3e26f21dbcac9acb767ccc9ebaa0f8d35bba3
Leobardo Ernesto Sanchez Cruz <Leobardo.Sanchez@uabc.edu.mx>
2021-02-25 15:59:05 -0800

    11 python junior can do version 3

---
+++ What's my acronym/What's_my_acronym.py
@@ -0,0 +1,18 @@
+
+def acronym(phrase):
+    a = ""
+    for words in phrase.split():
+        a = a + words[0]
+    return a
+
+def main():
+    print("insert a full meaning of organization or concepto to get is acronym")
+    print("entered by the user.")
+
+    text = input("Please enter your phrase: ")
+    text = text.upper()
+
+    answer = acronym(text)
+
+    print(str(answer))
+main()
